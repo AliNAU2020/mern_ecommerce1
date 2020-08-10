7ba5df04c44f22b0b2cfef94d71366d054840301
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-10 02:46:21 -0500

    Update HomeScreen.js

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import data from '../data'
 
 function HomeScreen(props){
     return <ul className = "products">
     {
       data.products.map(product =>
     
-      <li>
+      <li key = {product._id}>
           <div className = "product">
+          <Link to ={'/product/' + product._id}>
               <img className = "product-image" src = {product.image} alt = "product" />
+
+            </Link>
               <div className = "product-name">
                   <Link to ={'/product/' + product._id}>{product.name}</Link> 
               </div>
               <div className = "product-brand">{product.brand}</div>
-              <div className = "product-price">{product.price}</div>
-              <div className = "product-rating">{product.rating} {(product.numReviews)}</div>
+              <div className = "product-price">${product.price}</div>
+              <div className = "product-rating">{product.rating} Stars ({product.numReviews})</div>
           </div>
       </li>)
     } 
