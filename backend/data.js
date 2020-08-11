45821a98410aa1b5b98e2b912e9ffe0298886060
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-11 00:19:26 -0500

    Create data.js

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+    {
+        _id: '1',
+        name: 'Slim Work Backpack',
+        category: 'Backpacks',
+        image: '/images/b1.jpg',
+        price: 315,
+        brand: 'Briggs & Riley',
+        rating: 4.5,
+        numReviews: 10
+    },
+
+    {
+        _id: '2',
+        name: 'TSA ScanSmart Laptop Backpack',
+        category: 'Backpacks',
+        image: '/images/b2.jpg',
+        price: 150,
+        brand: 'SwissGear',
+        rating: 4.5,
+        numReviews: 10
+    },
+
+    {
+        _id: '3',
+        name: 'AntiTheft Work Backpack',
+        category: 'Backpacks',
+        image: '/images/b3.jpg',
+        price: 65,
+        brand: 'John Kart',
+        rating: 4.5,
+        numReviews: 10
+    },
+    
+    {
+        _id: '4',
+        name: 'Gym Duffle Bag',
+        category: 'Duffle Bags',
+        image: '/images/b4.jpg',
+        price: 75,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+
+    {
+        _id: '5',
+        name: 'Gym & Work Duffle Bag',
+        category: 'Duffle Bags',
+        image: '/images/b5.jpg',
+        price: 315,
+        brand: 'WolfWarriorX',
+        rating: 4.5,
+        numReviews: 10
+    },
+
+    {
+        _id: '6',
+        name: 'Travel Duffle Bag',
+        category: 'Duffle Bags',
+        image: '/images/b6.jpg',
+        price: 175,
+        brand: 'Wowbox',
+        rating: 5,
+        numReviews: 10
+    },
+]
+
+}
