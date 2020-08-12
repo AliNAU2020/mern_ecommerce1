dd230057add90b21836fabe23439f5b251d62b1a
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-12 01:35:16 -0500

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import Cookie from 'js-cookie';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
 
-const initialState ={};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState ={cart : {cartItems} };
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_
 const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
 export default store;
