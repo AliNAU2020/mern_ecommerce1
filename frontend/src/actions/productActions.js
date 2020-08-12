80b5d17abb98bf30c7b0cee33062d1dccc9ddd18
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-12 05:28:15 -0500

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,8 @@
-
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
-import axios from 'axios';
+import {
+    PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST
+  } from "../constants/productConstants"
+  import axios from 'axios';
 
 const listProducts = ( ) => async (dispatch) =>{
 try{
@@ -13,6 +15,32 @@ catch(error) {
 }
 }
 
+const saveProduct = (product) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+      const { userSignin: { userInfo } } = getState();
+      if (!product._id) {
+        const { data } = await axios.post('/api/products', product, {
+          headers: {
+            'Authorization': 'Bearer ' + userInfo.token
+          }
+        });
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+      } else {
+        const { data } = await axios.put('/api/products/' + product._id, product, {
+          headers: {
+            'Authorization': 'Bearer ' + userInfo.token
+          }
+        });
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+      }
+  
+    } catch (error) {
+  
+      dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
+    }
+  }
+  
 
 
 const detailsProduct = (productID) => (dispatch) => {
