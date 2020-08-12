9149284ed3f8649d02cf4fd609ea9788c51ee3b3
AliNAU2020 <31328423+AliNAU2020@users.noreply.github.com>
2020-08-11 19:10:39 -0500

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,7 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
 import axios from 'axios';
+
 const listProducts = ( ) => async (dispatch) =>{
 try{
     dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -11,4 +13,17 @@ catch(error) {
 }
 }
 
-export { listProducts }
+
+
+const detailsProduct = (productID) => (dispatch) => {
+    try{
+        dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productID});
+        const { data } =  axios.get("/api/products/" + productID);
+        dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data})
+    } catch (error) {
+        dispatch({type: PRODUCT_DETAILS_FAIL, payload: error.message});
+    }
+}
+
+
+export { listProducts, detailsProduct  }
